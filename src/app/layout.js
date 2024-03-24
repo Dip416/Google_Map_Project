42efe76e06d38c2026b0989c5e9936a14c98cbf0
diprana416 <diprana416@gmail.com>
2024-03-24 12:03:18 +0530

    Intialize Next js repo

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Google Map',
+  description: 'This is the google map app',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
